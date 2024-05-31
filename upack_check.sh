5415ff4d2c46fe555553119c0ce16cd11ffa01cb
Radik <safinrraf@ymail.com>
2024-05-31 16:27:18 +0200

    upack_check.sh: added logging to the file

--- upack_check.sh
+++ upack_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROGRAM=${0##*/} # the version
-VERSION="$PROGRAM v1.0.1"
+VERSION="$PROGRAM v1.0.3"
 LOG_TIME_FORMAT="%F %T"
 
 BASE_DIR=~/programm/data
@@ -10,6 +10,7 @@ ARCHIVE_FILE_TIMESTAMP=$(date +%Y%m%d%H%M%S)
 DEST_DIR=$BASE_DIR/DEST
 ARCHIVE_DIR=$BASE_DIR/ARCHIVE/$CURRENT_DATE
 CSV_FILE_NAME="FILE.csv"
+LOG_FILE=$BASE_DIR/ARCHIVE/CSV_LOG_$ARCHIVE_FILE_TIMESTAMP.log
 
 # ANSI color codes
 RED='\033[0;31m'
@@ -25,21 +26,25 @@ fi
 # Define the function
 function handle_error {
     echo "$(date +"$LOG_TIME_FORMAT") Caught an error with exit status $?" >&2
+    echo "$(date +"$LOG_TIME_FORMAT") Caught an error with exit status $?" >> $LOG_FILE
 }
 
 function loggging() {
     local message=$1
     echo "$(date +"$LOG_TIME_FORMAT") $message"
+    echo "$(date +"$LOG_TIME_FORMAT") $message" >> $LOG_FILE
 }
 
 function eeloggging() {
     local message=$1
     echo -e "$(date +"$LOG_TIME_FORMAT") ${RED}$message${NC}"
+    echo -e "$(date +"$LOG_TIME_FORMAT") ${RED}$message${NC}" >> $LOG_FILE
 }
 
 function ggloggging() {
     local message=$1
     echo -e "$(date +"$LOG_TIME_FORMAT") ${GREEN}$message${NC}"
+    echo -e "$(date +"$LOG_TIME_FORMAT") ${GREEN}$message${NC}" >> $LOG_FILE
 }
 
 # Set the error handler
@@ -75,7 +80,7 @@ echo $INPUT_SHA256_UPPER >> $INPUT_SHA256_UPPER_FILE
 
 unzip -P $INPUT_ZIP_PASSWORD $TEMP_DIR/*.zip -d $TEMP_DIR/
 #Archiving previous files
-#find $DEST_DIR -type f -name "*.csv" | tar -czvf $ARCHIVE_DIR/PEP_PREV_$ARCHIVE_FILE_TIMESTAMP.tar.gz --remove-files -T -
+#find $DEST_DIR -type f -name "*.csv" | tar -czvf $ARCHIVE_DIR/CSV_PREV_$ARCHIVE_FILE_TIMESTAMP.tar.gz --remove-files -T -
 
 mv $TEMP_DIR/*.csv $DEST_DIR/$CSV_FILE_NAME
 
