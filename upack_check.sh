9ba47aaa124ababc83f45fe05fc080b26bb9db80
Radik <safinrraf@ymail.com>
2024-06-01 12:02:25 +0200

    new v 1.0.4: changed a logic of archiving

--- upack_check.sh
+++ upack_check.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 PROGRAM=${0##*/} # the version
-VERSION="$PROGRAM v1.0.3"
+VERSION="$PROGRAM v1.0.4"
 LOG_TIME_FORMAT="%F %T"
 
-BASE_DIR=~/programm/data
 CURRENT_DATE=$(date +%Y%m%d)
 ARCHIVE_FILE_TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+BASE_DIR=~/programm/data
 DEST_DIR=$BASE_DIR/DEST
 ARCHIVE_DIR=$BASE_DIR/ARCHIVE/$CURRENT_DATE
+LOG_FILE=$ARCHIVE_DIR/CSV_LOG_$ARCHIVE_FILE_TIMESTAMP.log
 CSV_FILE_NAME="FILE.csv"
-LOG_FILE=$BASE_DIR/ARCHIVE/CSV_LOG_$ARCHIVE_FILE_TIMESTAMP.log
 
 # ANSI color codes
 RED='\033[0;31m'
@@ -23,12 +24,12 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-# Define the function
-function handle_error {
-    echo "$(date +"$LOG_TIME_FORMAT") Caught an error with exit status $?" >&2
-    echo "$(date +"$LOG_TIME_FORMAT") Caught an error with exit status $?" >> $LOG_FILE
-}
+# Check if directory exists
+if [ ! -d "$ARCHIVE_DIR" ]; then
+    mkdir $ARCHIVE_DIR
+fi
 
+# Define the functions
 function loggging() {
     local message=$1
     echo "$(date +"$LOG_TIME_FORMAT") $message"
@@ -47,6 +48,27 @@ function ggloggging() {
     echo -e "$(date +"$LOG_TIME_FORMAT") ${GREEN}$message${NC}" >> $LOG_FILE
 }
 
+function handle_error {
+    eeloggging "$(date +"$LOG_TIME_FORMAT") Caught an error with exit status $?"
+}
+
+function clear_tmp {
+    loggging "Start clearing"
+    rm -f $TEMP_DIR/*.csv
+    loggging "Removed CSV file from $TEMP_DIR"
+    rm -f $TEMP_DIR/*.zip
+    loggging "Removed ZIP file from $TEMP_DIR"
+    rm -f $TEMP_DIR/*.txt
+    loggging "Removed TXT file from $TEMP_DIR"
+    loggging "End clearing"
+}
+
+function show_dir_content {
+    local PARAMETER_DIR=$1
+    loggging "The content of $PARAMETER_DIR"
+    ls -halt $PARAMETER_DIR >> $LOG_FILE
+}
+
 # Set the error handler
 trap 'handle_error; exit 1' ERR
 
@@ -62,29 +84,24 @@ INPUT_SHA256_UPPER_FILE=$TEMP_DIR/sha56.txt
 ### Start the script
 
 loggging "Start $VERSION: $INPUT_SHA256_UPPER"
-loggging "BASE_DIR $BASE_DIR"
-loggging "TEMP_DIR $TEMP_DIR"
-loggging "DEST_DIR $DEST_DIR"
-loggging "CURRENT_DATE $CURRENT_DATE"
-
-# Check if directory exists
-if [ -d "$ARCHIVE_DIR" ]; then
-    loggging "ARCHIVE_DIR $ARCHIVE_DIR"
-else
-    mkdir $ARCHIVE_DIR
-fi
+loggging "BASE_DIR.......$BASE_DIR"
+loggging "DEST_DIR.......$DEST_DIR"
+loggging "ARCHIVE_DIR....$ARCHIVE_DIR"
+loggging "TEMP_DIR.......$TEMP_DIR"
+loggging "CURRENT_DATE...$CURRENT_DATE"
+loggging "|"
+loggging "|"
 
 cp $INPUT_FILE $TEMP_DIR/
 echo $INPUT_ZIP_PASSWORD >> $INPUT_ZIP_PASSWORD_FILE
 echo $INPUT_SHA256_UPPER >> $INPUT_SHA256_UPPER_FILE
-
+loggging "Created supplimentary files"
 unzip -P $INPUT_ZIP_PASSWORD $TEMP_DIR/*.zip -d $TEMP_DIR/
-#Archiving previous files
-#find $DEST_DIR -type f -name "*.csv" | tar -czvf $ARCHIVE_DIR/CSV_PREV_$ARCHIVE_FILE_TIMESTAMP.tar.gz --remove-files -T -
+loggging "Unzipped into $TEMP_DIR"
+mv $TEMP_DIR/*.csv $TEMP_DIR/$CSV_FILE_NAME
+show_dir_content $TEMP_DIR
 
-mv $TEMP_DIR/*.csv $DEST_DIR/$CSV_FILE_NAME
-
-SHA256_HASH=$(openssl dgst -sha256 $DEST_DIR/$CSV_FILE_NAME | cut -d' ' -f2 | tr '[:lower:]' '[:upper:]')
+SHA256_HASH=$(openssl dgst -sha256 $TEMP_DIR/$CSV_FILE_NAME | cut -d' ' -f2 | tr '[:lower:]' '[:upper:]')
 
 loggging "|"
 loggging "|"
@@ -93,17 +110,36 @@ loggging "CALC  SHA256_HASH $SHA256_HASH"
 loggging "|"
 loggging "|"
 
-#Archiving previous files
-#find $TEMP_DIR -type f -name "*" | tar -czvf $ARCHIVE_DIR/INPUT_$ARCHIVE_FILE_TIMESTAMP.tar.gz --remove-files -T -
-rm -f $TEMP_DIR/*.zip
-
+loggging "|"
+loggging "Checking the HASH"
+loggging "|"
 # Compare the strings
 if [ "$SHA256_HASH" == "$INPUT_SHA256_UPPER" ]; then
     ggloggging "SHA256_HASH OK"
     loggging "|"
+    loggging "|"
 else
     eeloggging "SHA256_HASH IS NOT CORRECT"
+    clear_tmp
+    eeloggging "EXIT"
     exit 1
 fi
+
+#Archiving previous files
+#find $DEST_DIR -type f -name "*.csv" | tar -czvf $ARCHIVE_DIR/CSV_PREV_$ARCHIVE_FILE_TIMESTAMP.tar.gz --remove-files -T -
+loggging "Archived previous CSV file"
+
+mv $TEMP_DIR/$CSV_FILE_NAME $DEST_DIR/$CSV_FILE_NAME
+loggging "Moved a new CSV to $DEST_DIR"
+
+#Archiving input files
+#find $TEMP_DIR -type f -name "*" | tar -czvf $ARCHIVE_DIR/INPUT_$ARCHIVE_FILE_TIMESTAMP.tar.gz --remove-files -T -
+loggging "Archived input files files"
+
+show_dir_content $DEST_DIR
+
+clear_tmp
+
+show_dir_content $TEMP_DIR
 
 loggging "End $VERSION: $INPUT_SHA256_UPPER"
